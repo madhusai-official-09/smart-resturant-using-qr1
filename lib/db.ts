534ebd04c1e67590263cb7cb62b9a8376736b521
Pitani Madhusayi krishna Maheshwara Prakash Rao <22221a4545@gmail.com>
2026-01-03 19:19:51 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,27 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGO_URI as string;
 
-let cached = (global as any).mongoose;
+if (!MONGODB_URI) throw new Error("MongoDB URI missing");
 
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
+export const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
 
-export async function connectDB() {
-  if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI missing");
-  }
-
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+  await mongoose.connect(MONGODB_URI);
+  console.log("MongoDB Connected");
+};
