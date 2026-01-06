a5c281310daafc3d4f292b725c966b18fcd2de18
Pitani Madhusayi krishna Maheshwara Prakash Rao <22221a4545@gmail.com>
2026-01-06 19:07:06 +0530

    Update Order.ts

--- lib/models/Order.ts
+++ lib/models/Order.ts
@@ -2,24 +2,24 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema(
   {
-    table: Number,
-    items: Array,
+    table: {
+      type: Number,
+      required: true,
+    },
+    items: [
+      {
+        name: String,
+        price: Number,
+      },
+    ],
     status: {
       type: String,
-      enum: ["Preparing", "Finished", "Cancelled"],
-      default: "Preparing",
+      default: "Served", // Preparing → Cooking → Served
     },
-    expiresAt: Date,
   },
   { timestamps: true }
 );
 
-OrderSchema.pre("save", function (next) {
-  if (!this.expiresAt) {
-    this.expiresAt = new Date(Date.now() + 10 * 60 * 1000);
-  }
-  next();
-});
-
 export default mongoose.models.Order ||
   mongoose.model("Order", OrderSchema);
+
